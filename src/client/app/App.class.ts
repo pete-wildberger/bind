2765c1cee19040511c91ebaa2f551f1a4f518414
Peter Wildberger <pete.wildberger@gmail.com>
2020-12-08 10:03:44 -0600

    setup canvas and context

--- src/client/app/App.class.ts
+++ src/client/app/App.class.ts
@@ -1,8 +1,21 @@
 export class App {
-	private context: CanvasRenderingContext2D | null;
+	private context: CanvasRenderingContext2D;
 	constructor() {
+		const canvas = this.setCanvas();
+		this.context = this.setContext(canvas);
+	}
+
+	private setCanvas(): HTMLCanvasElement {
 		const canvas = document.getElementById('view') as HTMLCanvasElement;
-		this.context = canvas.getContext('2d');
-		console.log(this.context);
+		canvas.height = 288;
+		canvas.width = 320;
+		canvas.style.pointerEvents = 'none';
+		return canvas;
+	}
+
+	private setContext(canvas: HTMLCanvasElement) {
+		const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+		context.imageSmoothingEnabled = false;
+		return context;
 	}
 }
